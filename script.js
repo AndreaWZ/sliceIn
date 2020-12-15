36fdde7a0802aa41ef9d9eb1f2c76834ab749901
Andrea Wu <lixianhyun@gmail.com>
2020-12-14 23:33:30 -0800

    add 'eventListener' to images variable

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const images = document.querySelectorAll("img");
+
+for(let i =0; i < images.length; i ++) {
+    images[i].addEventListener("click", function() {
+        console.log("Clicked!");
+    })
+}
+
